ddc94d93afa4a64e2e5e4211e32b9535792d5e58
shampi75 <shrikanth.hampiholi@gmail.com>
2026-01-08 16:26:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("triggred with hook");
+        System.out.println("trying webhook");
     }
 }
